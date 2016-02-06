3542af60fae43e8cb4108968547380f6233e0e9d
Connie Yuan <connieyuan92@gmail.com>
2016-02-06 12:24:32 -0800

    added access key as environment variables, added start script for heroku

--- models/simpleDB.js
+++ models/simpleDB.js
@@ -1,11 +1,13 @@
 /* Load all dependencies */
 var AWS = require('aws-sdk');
-var config = require('../config.json');
 var bcrypt = require('bcrypt-nodejs');
 
 /* AWS table names and AWS instances */
 var loginTable = "login_aid_buddy";
-AWS.config.loadFromPath('./config.json');
+var AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_KEY;
+AWS.config.update({region: 'us-west-2'});
+
 var simpledb = new AWS.SimpleDB();
 
 /* Authenticate user by finding the username in the user table */
